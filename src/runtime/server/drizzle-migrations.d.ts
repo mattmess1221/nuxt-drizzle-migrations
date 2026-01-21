fb3670bbdf478a99341096628ac3bc5b81fa475a
Matthew Messinger <mattmess1221@gmail.com>
2026-01-21 12:42:59 -0500

    feat: allow configuration of migration table and schema
    Will read from `drizzle.config` by default.

--- src/runtime/server/drizzle-migrations.d.ts
+++ src/runtime/server/drizzle-migrations.d.ts
@@ -2,6 +2,7 @@ declare module '#drizzle-migrations' {
   export { default as journal } from '#drizzle-migrations/journal'
 
   export const storageName: string
+  export const migrationsConfig: Partial<import('drizzle-orm/migrator').MigrationConfig>
 }
 
 declare module '#drizzle-migrations/journal' {
