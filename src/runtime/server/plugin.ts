942a011e0367ba7c5b046f1b22ebd9b2b189d4f1
Matthew Messinger <mattmess1221@gmail.com>
2026-01-24 20:08:22 -0500

    types(plugin): replace `Migration` with `MigrationMeta` from `drizzle-orm/migrator`

--- src/runtime/server/plugin.ts
+++ src/runtime/server/plugin.ts
@@ -1,3 +1,4 @@
+import type { MigrationMeta } from 'drizzle-orm/migrator'
 import { journal, storageName } from '#drizzle-migrations'
 // eslint-disable-next-line ts/ban-ts-comment
 // @ts-ignore - server types are incorrect during dev, but works fine in upstream nuxt projects
@@ -8,16 +9,9 @@ import { digest } from 'ohash'
 
 const logger = consola.withTag('drizzle-migrations')
 
-interface Migration {
-  sql: string[]
-  bps: boolean
-  folderMillis: number
-  hash: string
-}
-
 interface $Drizzle {
   dialect: {
-    migrate: (migrations: Migration[], session: any, config: Record<any, any>) => any
+    migrate: (migrations: MigrationMeta[], session: any, config: Record<any, any>) => any
   }
   session: any
 }
@@ -40,9 +34,9 @@ export default defineNitroPlugin(async (nitroApp) => {
  * Reads migration queries from the given Unstorage instance.
  * Taken from 'drizzle-orm/migrator' but modified to read from unstorage instead of fs.
  */
-async function readMigrationStorage(): Promise<Migration[]> {
+async function readMigrationStorage(): Promise<MigrationMeta[]> {
   const storage = useStorage<string>(`assets:${storageName}`)
-  const migrationQueries: Migration[] = []
+  const migrationQueries: MigrationMeta[] = []
   for (const journalEntry of journal.entries) {
     const migrationPath = `${journalEntry.tag}.sql`
     const query = await storage.getItem(migrationPath)
